9e190535028a094e8282a84037d514622a185b79
Frederico Fávaro <fredericofavaro@gmail.com>
2025-08-12 19:47:37 +0000

    aula 2

---
+++ data_types.py
@@ -0,0 +1,13 @@
+# print(type("22"))
+# print(type(22.3))
+# print(type(0))
+# print(type(True))
+# print(type(False))
+# print(type(str(22)))
+# int()
+# float()
+print(bool())
+
+#nome_aluno_tarde = input("Informe seu nome:")
+#print(type(int(nome)))
+
---
+++ string.py
@@ -0,0 +1,6 @@
+
+nome = "Fred"
+
+cpf = 748328572
+residencia = "apartamento x"
+print(f"Eu $, com cpf $, residente no endereço $....")
