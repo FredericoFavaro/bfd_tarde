2e52bfe701119cb9a58a6e3d66d50f0592b9e2ed
Frederico <fredericofavaro@gmail.com>
2025-08-13 19:57:20 +0000

    aula condicionais

---
+++ condicionais.py
@@ -0,0 +1,10 @@
+x = int(input("Informe o primeiro número: "))
+y = int(input("Informe o segundo número: "))
+
+
+
+if y != 0:
+    print(x/y)
+else:
+    print("O valor informado para o segundo numero tem que ser diferente de 0")
+
---
+++ mentoria01/data_types.py
@@ -0,0 +1,39 @@
+"""
+funcao que testa se cpf e valido na receita
+
+TIPOS DE DADOS
+- String (texto)
+- Numero - Inteiro
+         - Ponto flutuante (real)
+- Booleano (True / False)
+----------
+- lista
+- tupla
+- dicionario
+"""
+
+#casa = "ola"
+#print(type(casa))
+#print(2 < 5) 
+
+########
+#print(5+5)
+#print(5-2)
+#print(5*2)
+#print(5/5)
+#print(5//2)
+#print(5%2)
+
+""" dfsdfsdf
+==
+!=
+<
+>
+<=
+>=
+"""
+
+
+
+nome_usuario = input("Informe seu nome: ")
+print(f"o nome do usuario é",nome_usuario)
